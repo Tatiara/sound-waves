e41a3cb77837108fdd1b38d52cded8aac5170efb
Tatiara <thaisandrade83@gmail.com>
2017-07-12 15:33:02 -0300

    Iniciar classes do projeto

---
+++ musicsPoo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace musicsPoo
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (usuario.Text == "" || senha.Text == "") {
+                MessageBox.Show("Preencha todos os campos");
+            }
+            if (usuario.Text=="adim"&& senha.Text=="adim")
+            {
+                
+            }
+        }
+    }
+}
